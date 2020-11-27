63725078a32c23108402a22f8b727b7c31742ed7
EngMo974 <42417845+EngMo974@users.noreply.github.com>
2020-11-27 16:34:55 +0200

    fourth commit, design third screen, question: why the color of button doesn't change?

--- app/src/main/java/com/example/todolist/SignUp.java
+++ app/src/main/java/com/example/todolist/SignUp.java
@@ -1,17 +1,18 @@
 package com.example.todolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
+    private TextView toLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         try {
@@ -20,7 +21,14 @@ public class SignUp extends AppCompatActivity {
 
         }
 
-
-
+        toLogin = (TextView) findViewById(R.id.login);
+        toLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        }
+        );
     }
 }
